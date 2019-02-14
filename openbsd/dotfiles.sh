0352dd0d79a57321e2f8022fb3951efa19e7336a
Roman Zolotarev <hi@romanzolotarev.com>
2019-02-14 13:05:25 +0000

    update form, m, n, rssg_bench, dotfiles, profile

--- openbsd/dotfiles.sh
+++ openbsd/dotfiles.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 set -x
 
-ln -fsh	"$HOME/src/www/bin"			"$HOME/bin"
+ln -fsh	"$HOME/src/rgz/bin"			"$HOME/bin"
 ln -fsh	"$HOME/prv/pass"			"$HOME/.pass"
 ln -fsh	"$HOME/prv/ssh"				"$HOME/.ssh"
 
-ln -fs	"$HOME/src/www/openbsd/Xdefaults"	"$HOME/.Xdefaults"
-ln -fs	"$HOME/src/www/openbsd/cwmrc"		"$HOME/.cwmrc"
-ln -fs	"$HOME/src/www/openbsd/exrc"		"$HOME/.exrc"
-ln -fs	"$HOME/src/www/openbsd/gitconfig"	"$HOME/.gitconfig"
-ln -fs	"$HOME/src/www/openbsd/profile"		"$HOME/.profile"
-ln -fs	"$HOME/src/www/openbsd/tmux.conf"	"$HOME/.tmux.conf"
-ln -fs	"$HOME/src/www/openbsd/xsession"	"$HOME/.xsession"
+ln -fs	"$HOME/src/rgz/openbsd/Xdefaults"	"$HOME/.Xdefaults"
+ln -fs	"$HOME/src/rgz/openbsd/cwmrc"		"$HOME/.cwmrc"
+ln -fs	"$HOME/src/rgz/openbsd/exrc"		"$HOME/.exrc"
+ln -fs	"$HOME/src/rgz/openbsd/gitconfig"	"$HOME/.gitconfig"
+ln -fs	"$HOME/src/rgz/openbsd/profile"		"$HOME/.profile"
+ln -fs	"$HOME/src/rgz/openbsd/tmux.conf"	"$HOME/.tmux.conf"
+ln -fs	"$HOME/src/rgz/openbsd/xsession"	"$HOME/.xsession"
 
 mkdir -p "$HOME/.config/"
-ln -fs	"$HOME/src/www/openbsd/user-dirs.dirs"	"$HOME/.config/user-dirs.dirs"
+ln -fs	"$HOME/src/rgz/openbsd/user-dirs.dirs"	"$HOME/.config/user-dirs.dirs"
 
 mkdir -p "$HOME/.config/newsboat"
-ln -fs	"$HOME/src/www/blogroll.txt"		"$HOME/.config/newsboat/urls"
-ln -fs	"$HOME/src/www/openbsd/newsboat.conf"	"$HOME/.config/newsboat/config"
+ln -fs	"$HOME/src/rgz/blogroll.txt"		"$HOME/.config/newsboat/urls"
+ln -fs	"$HOME/src/rgz/openbsd/newsboat.conf"	"$HOME/.config/newsboat/config"
